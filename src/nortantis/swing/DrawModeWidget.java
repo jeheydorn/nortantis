6198b22ed2c20b5bcb64e356b7bacb43538456e5
Joseph <jeheydorn@gmail.com>
2026-02-09 18:34:41 -0700

    Cleaned up code in DrawModeWidget

--- src/nortantis/swing/DrawModeWidget.java
+++ src/nortantis/swing/DrawModeWidget.java
@@ -1,13 +1,10 @@
 package nortantis.swing;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class DrawModeWidget
 {
@@ -15,68 +12,20 @@ public class DrawModeWidget
 	private JToggleButton eraseModeButton;
 	private JToggleButton replaceModeButton;
 	private JToggleButton editModeButton;
-	private boolean includeReplaceButton;
-	private boolean includeEditModeButton;
-	private JPanel container;
+	private SegmentedButtonWidget segmentedWidget;
 
 	public DrawModeWidget(String drawTooltipWithoutKeyboardShortcut, String eraseTooltipWithoutKeyboardShortcut, boolean includeReplaceButton, String replaceTooltipWithoutKeyboardShortcut,
 			boolean includeEditModeButton, String editTooltipWithoutKeyboardShortcut, Runnable changeListener)
 	{
-		this.includeReplaceButton = includeReplaceButton;
-		this.includeEditModeButton = includeEditModeButton;
-
-		ActionListener modeListener = new ActionListener()
+		ActionListener modeListener = e ->
 		{
-			@Override
-			public void actionPerformed(ActionEvent e)
+			JToggleButton source = (JToggleButton) e.getSource();
+			if (!source.isSelected())
 			{
-				if (e != null && e.getSource() == eraseModeButton)
-				{
-					drawModeButton.setSelected(false);
-					replaceModeButton.setSelected(false);
-					editModeButton.setSelected(false);
-					if (!eraseModeButton.isSelected())
-					{
-						eraseModeButton.setSelected(true);
-					}
-					eraseModeButton.grabFocus();
-				}
-				else if (e != null && e.getSource() == replaceModeButton)
-				{
-					drawModeButton.setSelected(false);
-					eraseModeButton.setSelected(false);
-					editModeButton.setSelected(false);
-					if (!replaceModeButton.isSelected())
-					{
-						replaceModeButton.setSelected(true);
-					}
-					replaceModeButton.grabFocus();
-				}
-				else if (e != null && e.getSource() == editModeButton)
-				{
-					drawModeButton.setSelected(false);
-					eraseModeButton.setSelected(false);
-					replaceModeButton.setSelected(false);
-					if (!editModeButton.isSelected())
-					{
-						editModeButton.setSelected(true);
-					}
-					editModeButton.grabFocus();
-				}
-				else
-				{
-					// Draw button
-					eraseModeButton.setSelected(false);
-					replaceModeButton.setSelected(false);
-					editModeButton.setSelected(false);
-					if (!drawModeButton.isSelected())
-					{
-						drawModeButton.setSelected(true);
-					}
-					drawModeButton.grabFocus();
-				}
-				changeListener.run();
+				source.setSelected(true);
 			}
+			source.grabFocus();
+			changeListener.run();
 		};
 
 		drawModeButton = new JToggleButton("<html><u>D</u>raw</html>");
@@ -84,72 +33,40 @@ public class DrawModeWidget
 		drawModeButton.setSelected(true);
 		drawModeButton.addActionListener(modeListener);
 		drawModeButton.setMnemonic(KeyEvent.VK_D);
-		SwingHelper.reduceHorizontalMargin(drawModeButton);
 
 		replaceModeButton = new JToggleButton("<html><u>R</u>eplace</html>");
 		replaceModeButton.setToolTipText(replaceTooltipWithoutKeyboardShortcut + " (Alt+R)");
 		replaceModeButton.addActionListener(modeListener);
 		replaceModeButton.setMnemonic(KeyEvent.VK_R);
-		SwingHelper.reduceHorizontalMargin(replaceModeButton);
 
 		editModeButton = new JToggleButton("<html>Edi<u>t</u></html>");
 		editModeButton.setToolTipText(editTooltipWithoutKeyboardShortcut + " (Alt+T)");
 		editModeButton.addActionListener(modeListener);
 		editModeButton.setMnemonic(KeyEvent.VK_T);
-		SwingHelper.reduceHorizontalMargin(editModeButton);
 
 		eraseModeButton = new JToggleButton("<html><u>E</u>rase</html>");
 		eraseModeButton.setToolTipText(eraseTooltipWithoutKeyboardShortcut + " (Alt+E)");
 		eraseModeButton.addActionListener(modeListener);
 		eraseModeButton.setMnemonic(KeyEvent.VK_E);
-		SwingHelper.reduceHorizontalMargin(eraseModeButton);
+
+		List<JToggleButton> buttons = new ArrayList<>();
+		buttons.add(drawModeButton);
+		if (includeReplaceButton)
+		{
+			buttons.add(replaceModeButton);
+		}
+		if (includeEditModeButton)
+		{
+			buttons.add(editModeButton);
+		}
+		buttons.add(eraseModeButton);
+
+		segmentedWidget = new SegmentedButtonWidget(buttons);
 	}
 
 	public RowHider addToOrganizer(GridBagOrganizer organizer, String labelTooltip)
 	{
-		container = new JPanel();
-		container.setLayout(new WrapLayout(WrapLayout.LEFT));
-		// Remove the horizontal and vertical gaps from the border around the elements.
-		container.setBorder(BorderFactory.createEmptyBorder(-5, -5, -5, -5));
-		addOptionsToContainer(true, true, includeReplaceButton, includeEditModeButton);
-
-		return organizer.addLabelAndComponent("Mode:", labelTooltip, container);
-	}
-
-	private void addOptionsToContainer(boolean showDrawMode, boolean showEraseMode, boolean showReplaceMode, boolean showEditMode)
-	{
-		List<JToggleButton> visibleButtons = new ArrayList<>();
-
-		if (showDrawMode)
-		{
-			visibleButtons.add(drawModeButton);
-		}
-		if (showReplaceMode)
-		{
-			visibleButtons.add(replaceModeButton);
-		}
-		if (showEditMode)
-		{
-			visibleButtons.add(editModeButton);
-		}
-		if (showEraseMode)
-		{
-			visibleButtons.add(eraseModeButton);
-		}
-
-		for (JToggleButton button : visibleButtons)
-		{
-			container.add(button);
-		}
-
-		if (visibleButtons.size() > 0)
-		{
-			Optional<JToggleButton> optional = visibleButtons.stream().filter((button) -> button.isSelected()).findFirst();
-			if (!optional.isPresent())
-			{
-				visibleButtons.get(0).doClick();
-			}
-		}
+		return segmentedWidget.addToOrganizer(organizer, "Mode:", labelTooltip);
 	}
 
 	public boolean isDrawMode()
