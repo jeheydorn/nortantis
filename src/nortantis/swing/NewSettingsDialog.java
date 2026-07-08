2b73319e5d42685787f17f062af99bb853e56793
Joseph <jeheydorn@gmail.com>
2026-07-08 13:46:35 -0600

    Widened NewSettingsDialog.

--- src/nortantis/swing/NewSettingsDialog.java
+++ src/nortantis/swing/NewSettingsDialog.java
@@ -43,7 +43,7 @@ public class NewSettingsDialog extends JDialog
 	private JProgressBar progressBar;
 	private MapUpdater updater;
 	private MapEditingPanel mapEditingPanel;
-	private Dimension defaultSize = new Dimension(955, 750);
+	private Dimension defaultSize = new Dimension(975, 750);
 	private int amountToSubtractFromLeftAndRightPanels = 40;
 	private Timer progressBarTimer;
 	public final double cityFrequencySliderScale = 100.0 / SettingsGenerator.maxCityProbability;
